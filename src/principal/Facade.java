f48da1e6f25ab5094f9707615d7038dd7d332c3a
Dumaru <robinsonmu@unisabana.edu.co>
2018-09-07 15:11:02 -0500

    Estructura

---
+++ src/principal/Facade.java
@@ -0,0 +1,53 @@
+package principal;
+import java.util.ArrayList;
+
+import adapter.*;
+
+public class Facade {
+	private static ArrayList<Usuario> usuarios = new ArrayList<>();
+	
+	public boolean verificarUsuario()
+	{
+		boolean existe = false;
+		
+		return existe;
+	}
+	
+	public void crearRegistroRuta()
+	{
+		
+	}
+	
+	public void listarRutasConductor()
+	{
+		
+	}
+	
+	public void actualizarRuta() 
+	{
+		
+	}
+	
+	public void eliminarRuta()
+	{
+		
+	}
+
+	public void crearRegistroReserva()
+	{
+		
+	}
+	public void listarReservasPasajero()
+	{
+		
+	}
+	public void modificarReservaPasajero()
+	{
+		
+	}
+	public void eliminarReserva()
+	{
+		
+	}
+	
+}
