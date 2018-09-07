96ab8739741bdfb72bac8b1b1d5dd31c2a580af6
Dumaru <robinsonmu@unisabana.edu.co>
2018-09-07 15:44:34 -0500

    C

--- src/principal/Facade.java
+++ src/principal/Facade.java
@@ -2,20 +2,32 @@ package principal;
 import java.util.ArrayList;
 
 import adapter.*;
+import composite.*;
 
 public class Facade {
 	private static ArrayList<Usuario> usuarios = new ArrayList<>();
+	private static ArrayList<Ruta> rutas = new ArrayList<>();
 	
-	public boolean verificarUsuario(String correo, String pass)
-	{
-		boolean existe = false;
+	public int verificarUsuario(String correo, String pass)
+	{	
+		int tipoPasajero = 0;
 		for(Usuario us: usuarios) {
 			if(us.getCorreo().equals(correo) && us.getContrasena().equals(pass)) {
-				existe = true;
+				//index = usuarios.indexOf(us);
+				tipoPasajero = obtenerTipo(us);
 			}
 		}
-		
-		return existe;
+		return tipoPasajero;
+	}
+	private int obtenerTipo(Usuario us)
+	{
+		int tipoUsu = 0;
+		if(us instanceof Pasajero) {
+			tipoUsu = 1;
+		}else if (us instanceof Conductor){
+			tipoUsu = 2;
+		}
+		return tipoUsu;
 	}
 	
 	public void crearRegistroRuta(String nombre)
