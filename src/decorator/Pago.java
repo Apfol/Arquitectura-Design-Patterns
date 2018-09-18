29615fae7bccc066c57853dcb5fc907bb9e3f747
Andrés Fabián Ramos <andresfabi90@gmail.com>
2018-09-18 14:15:05 -0500

    Atributos en pago

--- src/decorator/Pago.java
+++ src/decorator/Pago.java
@@ -1,17 +1,23 @@
 package decorator;
 
-public abstract class PagosPSE implements Component{
+public class Pago implements Component{
+	
+	public String valor;
+	public String documentoConductor;
+	public String documentoPasajero;
 	
 	
+
 	@Override
 	public void establecerParametro(String parametros) {
 		// TODO Auto-generated method stub
 		
 	}
-	
+
 	@Override
 	public String obtenerParametros() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 }
