e193e2c7f755d63de0e1b102670f36f32cb79b2a
Andrés Fabián Ramos <andresfabi90@gmail.com>
2018-09-18 14:01:51 -0500

    Clases decorator

---
+++ src/decorator/Component.java
@@ -0,0 +1,6 @@
+package decorator;
+
+public interface Component {
+	public void establecerParametro(String parametros);
+	public String obtenerParametros();
+}
---
+++ src/decorator/PagosPSE.java
@@ -0,0 +1,16 @@
+package decorator;
+
+public abstract class PagosPSE implements Component{
+	
+	@Override
+	public void establecerParametro(String parametros) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String obtenerParametros() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
